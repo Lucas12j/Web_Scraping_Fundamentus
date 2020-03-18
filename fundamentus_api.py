c7a7716565aaef3bfcb9f86e9a7816ba093a24a4
Joao Sangue <JoaoSangue@users.noreply.github.com>
2020-03-18 16:23:47 -0300

    Por ser uma API, REST no caso, as requisições são HTTP e não é necessário o arquivo "papeis.txt". Um servidor flask foi implementado para cuidar das requisições. Arquivo de saída agora é retornado como resposta da requisição. ".gitignore" removido.

--- fundamentus_api.py
+++ fundamentus_api.py
@@ -1,67 +1,64 @@
+import csv
+import io
+import flask
 import requests
 from bs4 import BeautifulSoup
-import csv
+from flask_restful import Resource, Api
+
+app = flask.Flask(__name__)
+api = Api(app)
 
 class GetData(object):
 
-    def __init__(self):
+    def __init__(self, p):
         self.table = []
-        self.lista_papeis = []
-        self.create_lista()
+        self.papel = p
     
-    def create_lista(self):
-        papeis = open('papeis.txt', 'r')
-        for i in papeis:
-            self.lista_papeis.append(i)
-        self.get_data()
-
-
     def get_data(self):
-        for i in self.lista_papeis:
-            i = i.rstrip()
-            url = "https://www.fundamentus.com.br/detalhes.php?papel="+i
-            r = requests.get(url)
-            soup = BeautifulSoup(r.text, 'lxml')
+        self.papel = self.papel.rstrip()
+        url = "https://www.fundamentus.com.br/detalhes.php?papel="+self.papel
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'lxml')
 
-            dia = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(2) .data:nth-child(2) .oscil font"))))
-            mes = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(3) .data:nth-child(2) .oscil font"))))
-            trinta_dias = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(4) .data:nth-child(2) .oscil font"))))
-            doze_meses = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(5) .data:nth-child(2) .oscil font"))))
-            vinte = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(6) .data:nth-child(2) .oscil font"))))
-            dezenove = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(7) .data:nth-child(2) .oscil font"))))
-            dezoito = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(8) .data:nth-child(2) .oscil font"))))
-            dezesete = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(9) .data:nth-child(2) .oscil font"))))
-            dezeseis     = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(10) .data:nth-child(2) .oscil font"))))
-            quinze = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(11) .data:nth-child(2) .oscil font"))))
-            p_l = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(2) .data:nth-child(4) .txt")))
-            p_vp= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(3) .data:nth-child(4) .txt")))
-            p_ebit= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(4) .data:nth-child(4) .txt")))
-            psr= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(5) .data:nth-child(4) .txt")))
-            p_ativos= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(6) .data:nth-child(4) .txt")))
-            p_cap_giro= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(7) .data:nth-child(4) .txt")))
-            p_ativ_circ_liq= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(8) .data:nth-child(4) .txt")))
-            dy= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(9) .data:nth-child(4) .txt")))
-            ev_ebitida= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(10) .data:nth-child(4) .txt")))
-            ev_ebit= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(11) .data:nth-child(4) .txt")))
-            cresc_rec= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(12) .data:nth-child(4) .txt")))
-            lpa = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(2) .data:nth-child(6) .txt")))
-            vpa = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(3) .data:nth-child(6) .txt")))
-            marg_bruta = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(4) .data:nth-child(6) .txt")))
-            marg_ebit = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(5) .data:nth-child(6) .txt")))
-            marg_liq = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(6) .data:nth-child(6) .txt")))
-            ebit_ativo = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(7) .data:nth-child(6) .txt")))
-            roic = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(8) .data:nth-child(6) .txt")))
-            roe = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(9) .data:nth-child(6) .txt")))
-            liqu_corr = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(10) .data:nth-child(6) .txt")))
-            divBr_patrim = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(11) .data:nth-child(6) .txt")))
-            giro_ativos = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(12) .data:nth-child(6) .txt")))
+        dia = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(2) .data:nth-child(2) .oscil font"))))
+        mes = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(3) .data:nth-child(2) .oscil font"))))
+        trinta_dias = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(4) .data:nth-child(2) .oscil font"))))
+        doze_meses = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(5) .data:nth-child(2) .oscil font"))))
+        vinte = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(6) .data:nth-child(2) .oscil font"))))
+        dezenove = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(7) .data:nth-child(2) .oscil font"))))
+        dezoito = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(8) .data:nth-child(2) .oscil font"))))
+        dezesete = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(9) .data:nth-child(2) .oscil font"))))
+        dezeseis = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(10) .data:nth-child(2) .oscil font"))))
+        quinze = self.cleaner(self.corrigir(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(11) .data:nth-child(2) .oscil font"))))
+        p_l = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(2) .data:nth-child(4) .txt")))
+        p_vp= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(3) .data:nth-child(4) .txt")))
+        p_ebit= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(4) .data:nth-child(4) .txt")))
+        psr= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(5) .data:nth-child(4) .txt")))
+        p_ativos = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(6) .data:nth-child(4) .txt")))
+        p_cap_giro = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(7) .data:nth-child(4) .txt")))
+        p_ativ_circ_liq= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(8) .data:nth-child(4) .txt")))
+        dy= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(9) .data:nth-child(4) .txt")))
+        ev_ebitida= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(10) .data:nth-child(4) .txt")))
+        ev_ebit= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(11) .data:nth-child(4) .txt")))
+        cresc_rec= self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(12) .data:nth-child(4) .txt")))
+        lpa = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(2) .data:nth-child(6) .txt")))
+        vpa = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(3) .data:nth-child(6) .txt")))
+        marg_bruta = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(4) .data:nth-child(6) .txt")))
+        marg_ebit = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(5) .data:nth-child(6) .txt")))
+        marg_liq = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(6) .data:nth-child(6) .txt")))
+        ebit_ativo = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(7) .data:nth-child(6) .txt")))
+        roic = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(8) .data:nth-child(6) .txt")))
+        roe = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(9) .data:nth-child(6) .txt")))
+        liqu_corr = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(10) .data:nth-child(6) .txt")))
+        divBr_patrim = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(11) .data:nth-child(6) .txt")))
+        giro_ativos = self.cleaner(str(soup.select(".center .clearfix .w728:nth-child(4) tr:nth-child(12) .data:nth-child(6) .txt")))
 
-            try:
-                self.table.append([i, dia, mes, trinta_dias, doze_meses,vinte ,dezenove,dezoito, dezesete, dezeseis, quinze, p_l, p_vp, p_ebit, psr, p_ativos, p_cap_giro, p_ativ_circ_liq, dy, ev_ebitida, ev_ebit, 
-                cresc_rec, lpa, vpa, marg_bruta, marg_ebit, marg_liq,ebit_ativo, roic, roe, liqu_corr, divBr_patrim, giro_ativos])
-            except:
-                print(f"Ausência de dados em: {i}")
-        self.export_csv()         
+        try:
+            self.table.append([self.papel, dia, mes, trinta_dias, doze_meses,vinte ,dezenove,dezoito, dezesete, dezeseis, quinze, p_l, p_vp, p_ebit, psr, p_ativos, p_cap_giro, p_ativ_circ_liq, dy, ev_ebitida, ev_ebit, 
+            cresc_rec, lpa, vpa, marg_bruta, marg_ebit, marg_liq,ebit_ativo, roic, roe, liqu_corr, divBr_patrim, giro_ativos])
+            return "Dados Encontrados", 302
+        except:
+            return "Ausência de dados", 417
     
     def cleaner(self, linha):
         valor = ""
@@ -85,10 +82,30 @@ class GetData(object):
         "CRESC. REC (5A) (%)","LPA","VPA","MARG.BRUTA (%)","MARG.EBIT (%)","MARG.LÍQUIDA (%)","EBIT/ATIVO (%)","ROIC (%)","ROE (%)","LIQUIDEZ CORR","DIV BR/PATRIM","GIRO ATIVOS"]]
         for i in self.table:
             gaveta.append(i)
-        with open('dados.csv', 'w', newline='', encoding='utf-8') as csvfile:
-            file_csv = csv.writer(csvfile)
-            for i in gaveta:
-                file_csv.writerow(i)
-        print("Processo Terminado")
+        
+        si = io.StringIO()
+        writer = csv.writer(si)
+        for i in gaveta:
+            writer.writerow(i)
+        
+        return [si.getvalue(), 200]
 
-GetData()
+
+
+class ApiRoutes(Resource):
+    def get(self, papel):
+        scraper = GetData(papel.upper())
+        response, status = scraper.get_data()
+        if status == 302:
+            response, status = scraper.export_csv()
+
+        response = flask.make_response(response, status)
+        response.headers.set('content-type', 'text/csv')
+        response.headers.set('Content-Disposition', 'attachment', filename=papel+'.csv')
+        return response
+
+
+api.add_resource(ApiRoutes, "/<string:papel>")
+
+if __name__ == '__main__':
+    app.run()
